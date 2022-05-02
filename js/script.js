50dd85de7bf4917291f6c0b91352ebbed8d63bcc
Kenny Le <99355017+Kenny-Le-281@users.noreply.github.com>
2022-05-02 17:26:48 +0000

    changes in index.html and script.js

--- js/script.js
+++ js/script.js
@@ -14,8 +14,23 @@ if (navigator.serviceWorker) {
 }
 
 /**
- * This function displays an alert.
+ * This function determins museum admission price
  */
-function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+function checked() {
+  // input
+  const sunday = document.getElementById("sunday").checked
+  const monday = document.getElementById("monday").checked
+  const tuesday = document.getElementById("tuesday").checked
+  const wednesday = document.getElementById("wednesday").checked
+  const thursday = document.getElementById("thursday").checked
+  const friday = document.getElementById("friday").checked
+  const saturday = document.getElementById("saturday").checked
+  var age = document.getElementById("age").value
+
+  // process and output
+  if (tuesday == true || thursday == true || age > 12 && age < 21) {
+    document.getElementById("museum").innerHTML = 'You are eligible for student pricing!'
+  } else {
+    document.getElementById("museum").innerHTML = 'You must pay regular price.'
+  }
 }
